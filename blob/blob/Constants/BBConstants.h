b2af54667fcb1cab73636c4288574e23c1286b56
Sam Yang <syangam@gmail.com>
2014-08-05 14:39:17 -0500

    add BBConstants and set initial background color to gray

---
+++ blob/blob/Constants/BBConstants.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+static NSString * const MY_BLOB_TITLE = @"My Blob";
+static NSString * const CLOSET_TITLE = @"Closet";
+static NSString * const SECRET_LANGUAGE_TITLE = @"Secret Language";
+
+@interface BBConstants : NSObject
++ (UIColor *)lightGrayBackgroundColor;
++ (UIColor *)lightGrayTabBackgroundColor;
+@end
---
+++ blob/blob/Constants/BBConstants.m
@@ -0,0 +1,15 @@
+#import "BBConstants.h"
+
+
+@implementation BBConstants
+
++ (UIColor *)lightGrayBackgroundColor {
+    return [UIColor colorWithRed:248.0f/255 green:248.0f/255 blue:248.0f/255 alpha:1.0f];
+}
+
++ (UIColor *)lightGrayTabBackgroundColor {
+    return [UIColor colorWithRed:249.0f/255 green:248.0f/255 blue:246.0f/255 alpha:1.0f];
+}
+
+
+@end
