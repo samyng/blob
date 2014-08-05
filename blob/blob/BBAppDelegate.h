170ae943b89b71b8cf20174ced21a23def0f143f
Sam Yang <syangam@gmail.com>
2014-08-05 14:20:43 -0500

    create Xcode project

---
+++ blob/blob/BBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BBAppDelegate.h
+//  blob
+//
+//  Created by Sam Yang on 8/5/14.
+//  Copyright (c) 2014 Crazy Machine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ blob/blob/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  blob
+//
+//  Created by Sam Yang on 8/5/14.
+//  Copyright (c) 2014 Crazy Machine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BBAppDelegate class]));
+    }
+}
