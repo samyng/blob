d4f748c8184130cfd9917338b84768d4845e9fc6
Sam Yang <syangam@gmail.com>
2014-08-06 18:27:08 -0500

    add accessory files back

---
+++ blob/blob/Controllers/Cells/BBAccessoryCollectionCell.h
@@ -0,0 +1,14 @@
+//
+//  BBAccessoryCollectionCell.h
+//  blob
+//
+//  Created by Sam Yang on 8/5/14.
+//  Copyright (c) 2014 Crazy Machine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BBAccessory.h"
+
+@interface BBAccessoryCollectionCell : UICollectionViewCell
+- (void)configureWithAccessory:(BBAccessory *)accessory;
+@end
