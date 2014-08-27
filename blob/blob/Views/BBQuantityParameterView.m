70b6c90b9db50df994dc5d6dfe001fc64e38c794
Sam Yang <syangam@gmail.com>
2014-08-27 12:24:34 -0500

    can enter numbers in quantity parameter view

--- blob/blob/Views/BBQuantityParameterView.m
+++ blob/blob/Views/BBQuantityParameterView.m
@@ -10,7 +10,13 @@
 #import "BBLanguageBlock+Configure.h"
 #import "BBLanguageBlockView.h"
 
+@interface BBQuantityParameterView () <UITextFieldDelegate>
+@property (strong, nonatomic) UITextField *quantityTextField;
+@end
+
 @implementation BBQuantityParameterView
+
+#pragma mark - Lifecycle
 
 - (void)awakeFromNib
 {
@@ -27,10 +33,53 @@
     return self;
 }
 
+#pragma mark - Setup
+
 - (void)setup
 {
     self.layer.cornerRadius = BLOB_QUANTITY_CORNER_RADIUS;
+    CGRect textFieldFrame = CGRectInset(self.bounds, 10.0f, 0.0f);
+    self.quantityTextField = [[UITextField alloc] initWithFrame:textFieldFrame];
+    [self addSubview:self.quantityTextField];
+    self.quantityTextField.delegate = self;
+    self.quantityTextField.returnKeyType = UIReturnKeyDone;
+    self.quantityTextField.keyboardType = UIKeyboardTypeNumberPad;
 }
+
+#pragma mark - Text Field Methods
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    if ([string rangeOfCharacterFromSet:[[NSCharacterSet decimalDigitCharacterSet] invertedSet]].location != NSNotFound)
+    {
+        return NO;
+    }
+    
+    NSUInteger newLength = [textField.text length] + [string length] - range.length;
+    if (newLength > 2)
+    {
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    textField.textAlignment = NSTextAlignmentLeft;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField
+{
+    textField.textAlignment = NSTextAlignmentCenter;
+}
+
+#pragma mark - Override Methods
 
 - (BOOL)acceptsBlockView:(BBLanguageBlockView *)blockView
 {
